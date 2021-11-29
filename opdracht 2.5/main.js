4a5881ae64330d663d8eb24505d9efd28c0783b9
Dhiradj <90622013+DhiradjT@users.noreply.github.com>
2021-11-29 16:39:07 +0100

    JS opdracht 2.5 en 2.6

---
+++ opdracht 2.5/main.js
@@ -0,0 +1,21 @@
+//2.5 vergelijkingen //
+
+console.log("file loaded");
+(1 < 4) = true
+(4 > 6) = false
+('A' < 'g') = true
+(5 == '5') = true 
+(5 != '5') = false 
+(421 >= 422) = false 
+(8 === '8') = true
+(8 !== '8') = false
+
+
+(x < y) = true
+(x === 5) = true
+(y === 'Z') = false
+(y !== z) = true
+(x+3 === y) = true
+(x+3+'hoi' === y+'hoi') = true
+(y+z === 12) = false 
+
